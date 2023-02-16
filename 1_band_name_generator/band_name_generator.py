3a2bb8fb8817add929a1f517e7984c40a3adb951
josefkong <josef.kong@outlook.com>
2023-02-16 13:33:58 -0300

    String Concatenation

---
+++ 1_band_name_generator/band_name_generator.py
@@ -0,0 +1,9 @@
+print("Welcome to the Band Name Generator!")
+
+city = input("What's the name of the city you grew up in?\n")
+
+pet = input("What's your first pet's name?\n")
+
+band_name = city + " " + pet
+
+print("Your band name should be " + band_name)
